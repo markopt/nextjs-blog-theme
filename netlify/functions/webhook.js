ad100fc45806eb01e4884c1b679af48896a0701a
Mark Huntley <markhuntley9@gmail.com>
2023-02-24 15:39:24 -0800

    changing webhook for split events

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -4,6 +4,28 @@ const handler = async (event) => {
     console.log(event.body)
     return {
       statusCode: 200,
+      const endpoint = 'https://events.split.io/api/events';
+      const url = new URL(endpoint);
+      url.searchParams.set('ts', event.timestamp);
+      const splitEvent = {
+        eventTypeId: event.body.event,
+        environmentName: 'Prod-Default',
+        trafficTypeName: 'user',
+        key: event.body.visitorId,
+        timestamp: new Date(event.timestamp).getTime(),
+        // value: event.properties.total,
+        properties: event.body.properties
+      };
+      console.log(splitEvent);
+      const res = await fetch(url.toString(), {
+        body: JSON.stringify(splitEvent),
+        headers: new Headers({
+          Authorization: 'Bearer robnevfsd5lrn9maicfmp4ipkb5s2lt459i6',
+          'Content-Type': 'application/json'
+        }),
+        method: 'post'
+      });
+      return await res.text();
       // body: JSON.stringify({ message: `Hello ${subject}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
