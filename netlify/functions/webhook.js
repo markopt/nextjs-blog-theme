e3a9f4830a0f24e1c7294bba0c8a681523854cc1
Mark Huntley <markhuntley9@gmail.com>
2023-02-24 17:44:05 -0800

    removing webhook features

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -2,35 +2,33 @@
 const handler = async (event) => {
   try {
     console.log(event.body)
-    return {
-      statusCode: 200,
-      const endpoint = 'https://events.split.io/api/events';
-      const url = new URL(endpoint);
-      url.searchParams.set('ts', event.timestamp);
-      const splitEvent = {
-        eventTypeId: event.body.accountId,
-        environmentName: 'Prod-Default',
-        trafficTypeName: 'user',
-        key: event.body.visitorId,
-        timestamp: new Date(event.timestamp).getTime(),
-        // value: event.properties.total,
-        properties: event.body.properties
-      };
-      console.log(splitEvent);
-      const res = await fetch(url.toString(), {
-        body: JSON.stringify(splitEvent),
-        headers: new Headers({
-          Authorization: 'Bearer robnevfsd5lrn9maicfmp4ipkb5s2lt459i6',
-          'Content-Type': 'application/json'
-        }),
-        method: 'post'
-      });
-      return await res.text();
-      // body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    // statusCode: 200,
+    const endpoint = 'https://events.split.io/api/events';
+    const url = new URL(endpoint);
+    url.searchParams.set('ts', event.body.timestamp);
+    const splitEvent = {
+      eventTypeId: event.body.accountId,
+      environmentName: 'Prod-Default',
+      trafficTypeName: 'user',
+      key: event.body.visitorId,
+      timestamp: new Date(event.timestamp).getTime(),
+      // value: event.properties.total,
+      properties: event.body.properties
+    };
+    console.log(splitEvent);
+    const res = await fetch(url.toString(), {
+      body: JSON.stringify(splitEvent),
+      headers: new Headers({
+        Authorization: 'Bearer robnevfsd5lrn9maicfmp4ipkb5s2lt459i6',
+        'Content-Type': 'application/json'
+      }),
+      method: 'post'
+    });
+    return await res.text();
+    // body: JSON.stringify({ message: `Hello ${subject}` }),
+    // // more keys you can return:
+    // headers: { "headerName": "headerValue", ... },
+    // isBase64Encoded: true,
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
