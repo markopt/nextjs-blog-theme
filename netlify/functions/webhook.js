81317e4838f9cd1cb721924038c879626219d1a2
Mark Huntley <markhuntley9@gmail.com>
2023-02-24 18:22:15 -0800

    adding logging

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,8 +1,9 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
   try {
-    console.log(event.body)
-    const payload = JSON.parse(event.body)
+    console.log(event.body);
+    const payload = JSON.parse(event.body);
+    console.log(payload);
     // statusCode: 200,
     const endpoint = 'https://events.split.io/api/events';
     const url = new URL(endpoint);
